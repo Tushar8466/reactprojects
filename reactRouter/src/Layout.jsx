e86351c6d4a6b8bc44bb3b33b76609a41b997518
tusharsingla22222-source <tusharsingla22222@gmail.com>
2026-01-04 13:00:49 +0530

    made a simple working website using react router

--- reactRouter/src/Layout.jsx
+++ reactRouter/src/Layout.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import Header  from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
 import { Outlet } from 'react-router-dom'
 
 function Layout() {
   return (
     <>
-    <Header />
-    <Outlet />
-    <Footer />
+      <Header />
+      <Outlet />
+      <Footer />
     </>
   )
 }
--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+import Github , { githubInfoLoader } from './components/Github/Github.jsx'
+
 
 // const router = createBrowserRouter([
 //   {
@@ -36,6 +38,8 @@ const router = createBrowserRouter(
       <Route path='' element={<Home />} />
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route loader={githubInfoLoader} path='github' element={<Github />} />
     </Route>
   )
 )
