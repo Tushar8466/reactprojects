f6b6cb1fd44b9975bb694443240ad5d2c8541604
tusharsingla22222-source <tusharsingla22222@gmail.com>
2026-01-05 10:34:23 +0530

    Add themeSwitcher project with theme toggle fixes

---
+++ themeSwitcher/src/contexts/theme.js
@@ -0,0 +1,14 @@
+import { useContext , createContext } from "react";
+
+export const ThemeContext = createContext({
+    themeMode: "light", // We can variable values
+    darkTheme : () => {}, // As well as we can give methods
+    lightTheme : () => {},
+});
+
+
+export const ThemeProvider = ThemeContext.Provider
+
+export default function useTheme(){
+    return useContext(ThemeContext);
+}
